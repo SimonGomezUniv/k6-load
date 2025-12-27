7ab500495d97381d037c2b44b5dbaf640997b983
simon.gomez <simon.gomez>
2025-12-27 22:30:30 +0100

    new script version

---
+++ run_script.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+k6 run --out influxdb=http://localhost:8086/k6 script.js
--- script.js
+++ script.js
@@ -2,24 +2,42 @@ import http from 'k6/http';
 import { sleep, check, group } from 'k6';
 
 export const options = {
-  vus: 2000,
-  duration: '300s'
+  scenarios: {
+    shared_iter_scenario: {
+      executor: 'shared-iterations',
+      vus: 100,
+      iterations: 1000,
+      startTime: '0s',
+    },
+    per_vu_scenario: {
+      executor: 'per-vu-iterations',
+      vus: 100,
+      iterations: 10,
+      startTime: '10s',
+    },
+  },
 };
 
 export default function() {
   group('homepage tests', () => {
-    let res = http.get('http://localhost:8080', {
-      tags: {
-        name: 'test-local',
-        endpoint: 'local',
-        method: 'get',
-        env: 'local',
-        team: 'platform',
-        version: 'v1.0',
-        expected_response: 'true'
-      }
-    });
-    check(res, { "status is 200": (res) => res.status === 200 });
+    for (let i = 0; i < 100; i++) { 
+      let res = http.get('http://localhost:8080', {
+        tags: {
+          name: 'test-local',
+          endpoint: 'local',
+          method: 'get',
+          env: 'local',
+          team: 'platform',
+          version: 'v1.0',
+          expected_response: 'true'
+        }
+      });
+      check(res, { "status is 200": (res) => res.status === 200 });
+    }
     sleep(1);
   });
+}
+
+export function teardown() {
+  sleep(5);
 }
